a634effd63f8fbb4a03be7fe9a914876b2ae2d56
Himanshu Ramteke <himanshuramteke007@gmail.com>
2024-09-03 12:34:52 +0530

    added solution

---
+++ 9.QuickSort/partitionElement.js
@@ -0,0 +1,27 @@
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+function partition(arr, pivotIndex) {
+    let pivotElement = arr[pivotIndex];
+    let L = 0, R = arr.length - 1;
+    let i = L - 1;
+    //swap the pivot on the last index
+    swap(arr, pivotIndex, R);
+    let j = L;
+    while(j <= R-1) {
+        if(arr[j] <= pivotElement) {
+            i++;
+            swap(arr, i, j);
+        }
+        j++;
+    }
+    swap(arr, i+1, R);
+}
+
+let arr = [1, 6, 3, 8, 9, 2, 5];
+pivotIndex = 2;
+partition(arr, pivotIndex);
+console.log(arr);
