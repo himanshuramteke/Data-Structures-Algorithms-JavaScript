11fc1a797cf478d9e78e1394202b12011320bb76
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-04-05 10:07:07 +0530

    Added monotonic stack problem

---
+++ 13.Stacks/monotonicStack/largestRectangleinHistogram.js
@@ -0,0 +1,30 @@
+/**
+ * Leetcode 84. Largest Rectangle in Histogram(https://leetcode.com/problems/largest-rectangle-in-histogram/)
+ * TC: O(n);
+ * SC: O(n);
+ */
+
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+    let st = [];
+    let n = heights.length;
+    st.push(0);
+    let ans = -1;
+    for(let i=1; i<n; i++) {
+        while(st.length  > 0 && heights[i] < heights[st[st.length - 1]]) {
+            let x = st[st.length - 1];
+            st.pop();
+            ans = Math.max(ans, heights[x] * (i - ((st.length > 0) ? st[st.length - 1] : -1) -1));
+        }
+        st.push(i);
+    }
+    while(!(st.length == 0)) {
+        let x = st[st.length - 1];
+        st.pop();
+        ans = Math.max(ans, heights[x] * (n - ((st.length > 0) ? st[st.length - 1] : -1) -1));
+    }
+    return ans;
+};
